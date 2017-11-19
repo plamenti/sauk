1823ecb347a868d994ed8feb3fad6ae22e9e1e1c
plamenti <plamenti_@abv.bg>
2017-11-19 11:39:15 +0200

    Add initial test project

---
+++ VisualStudioTestProject/TestDemoSauk/TestAjaxControls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDemoSauk
+{
+    [TestClass]
+    public class TestAjaxControls
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
