08a345f3b9f6eb1c3bdd683c4d7e2781d18d8f8d
plamenti <plamenti_@abv.bg>
2017-11-19 12:03:32 +0200

    Add TheRadNumericTextBoxPageExistsTest

--- VisualStudioTestProject/TestDemoSauk/TestAjaxControls.cs
+++ VisualStudioTestProject/TestDemoSauk/TestAjaxControls.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace TestDemoSauk
 {
     [TestClass]
     public class TestAjaxControls
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IWebDriver driver;
+        private string baseURL;
+        private const string PathToChromeDriver = @"D:\Drivers";
+
+        [TestInitialize]
+        public void TestInit()
         {
+            this.driver = new ChromeDriver(PathToChromeDriver);
+            baseURL = "https://www.telerik.com/";
+        }
+        
+        [TestMethod]
+        public void TheRadNumericTextBoxPageExistsTest()
+        {
+            this.driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.LinkText("Demos")).Click();
+            driver.FindElement(By.LinkText("Launch ASP.NET AJAX demos")).Click();
+            driver.FindElement(By.LinkText("NumericTextBox")).Click();
+
+            Assert.AreEqual("ASP.NET AJAX NumericTextBox Examples | RadNumericTextBox component", driver.Title); 
+        }
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            this.driver.Close();
+            this.driver.Dispose();
         }
     }
 }
